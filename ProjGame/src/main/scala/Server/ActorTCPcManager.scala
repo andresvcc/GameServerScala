fd8d4828b7b22ef4d3bccfc4b12ff924274aa338
andresvcc <43545168+andresvcc@users.noreply.github.com>
2019-02-15 12:58:57 +0100

    correcion de la linea 25 en ActorTCPcManager, remote ahora se envia como string

--- ProjGame/src/main/scala/Server/ActorTCPcManager.scala
+++ ProjGame/src/main/scala/Server/ActorTCPcManager.scala
@@ -22,7 +22,7 @@ class ActorTCPcManager(address: String, port: Int) extends Actor {
     // en el caso de recibir una conexion
     case Connected(remote, local) =>
       // handler es una instancia del modelo ActorTCPcHandler
-      val handler = context.actorOf(Props(classOf[ActorTCPcHandler], remote))
+      val handler = context.actorOf(Props(classOf[ActorTCPcHandler], s"$remote"))
       println(s"New connection: $local -> $remote")
       // se agrega el nuevo usuario a la lista de usuarios sin identificar
       // en este contexto sender es el ActorRef  y sender.path.name es el identificador de conexion de usuario
